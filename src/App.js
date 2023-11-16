d1a17b2fe1c351541c01f07491f2d5bd2128874e
Gautham <gauthammk2099@gmail.com>
2023-11-17 03:18:01 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Import your CSS file
 
-function App() {
+const App = () => {
+  const [teamNumber, setTeamNumber] = useState('');
+  const [responseData, setResponseData] = useState(null);
+
+  const handleInputChange = (event) => {
+    setTeamNumber(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Make a cURL request to the Google Sheets API
+    const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/1htzgArSyLu9kyl6Ed1SB4v_w5jiAn8oof7icfOzc1l8/values/B${teamNumber}:D${teamNumber}`;
+    const accessToken = 'ya29.a0AfB_byD_Z-zrtzFXkVIv24f9LlEax2lab3mf1thSKFetcjpfQ6gWFw9qB3MGOzyTVe_cqzPx-KtSKgUCwl_tl6S42mUG-nH8Hd964JRP6_jJV9nlZmH2Cz7eGYhhitLt3wHJk_DXAVWMYhGv4qA4Cma_8mcC32MQ5wosaCgYKAcESAQ8SFQHGX2MiFc9qFC8tXn2Kyaazu1MrTA0171';
+    fetch(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Store the response data in state
+        setResponseData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="heading">Flag <br></br>Wars</h1>
+      <p className="sub-heading">Brought to you by Team Technoinformals</p>
+      <div className="input-container">
+      <label htmlFor="teamNumber">Enter Your Team Number:</label>
+      <input
+        type="text"
+        id="teamNumber"
+        value={teamNumber}
+        onChange={handleInputChange}
+      />
+    </div>
+    <button className="submit-button" onClick={handleSubmit}>Submit</button>
+    <div className='responses'>
+      {responseData && (
+        <div>
+          <p>Values from Google Sheets API Response:</p>
+          <div>
+            {responseData.values[0].map((value, index) => (
+              <span key={index}>{value} </span>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
     </div>
   );
-}
+};
 
 export default App;
